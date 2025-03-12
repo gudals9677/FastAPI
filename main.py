03930bb941b3b7da8a8378838dd5d7634d59f53a
gudals <gudals@gmail.com>
2025-03-12 23:58:36 +0900

    crud

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI, Body, HTTPException
-from pydantic import BaseModel
+from typing import List
+
+from fastapi import FastAPI, Body, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+from database.connection import get_db
+from database.orm import ToDo
+from database.repository import get_todos, get_todo_by_todo_id, create_todo, update_todo, delete_todo
+from schema.request import CreateToDoRequest
+from schema.response import ToDoListSchema, ToDoSchema
 
 app = FastAPI()
 
@@ -10,17 +18,17 @@ def health_check():
 
 todo_data = {
     1: {
-        "id": 1,
+        "todo_id": 1,
         "contents": "Test FastAPI",
         "is_done": True,
     },
     2: {
-        "id": 2,
+        "todo_id": 2,
         "contents": "Test FastAPI",
         "is_done": False,
     },
     3: {
-        "id": 3,
+        "todo_id": 3,
         "contents": "Test FastAPI",
         "is_done": False,
     },
@@ -28,51 +36,69 @@ todo_data = {
 
 # GET API(전체 조회)
 @app.get("/todos", status_code=200)
-def get_todos_handler(order: str):
-    # todo_data values push to List
-    ret = list(todo_data.values())
+def get_todos_handler(
+        order: str,
+        session: Session = Depends(get_db),
+) -> ToDoListSchema:
+    todos: List[ToDo] = get_todos(session=session)
     if order == "DESC":
-        return ret[::-1]
-    return ret
+        return ToDoListSchema(
+        todos = [ToDoSchema.from_orm(todo) for todo in todos[::-1]]
+    )
+    return ToDoListSchema(
+        todos = [ToDoSchema.from_orm(todo) for todo in todos]
+    )
 
 # GET API(단일 조회)
-@app.get("/todos/{todos_id}", status_code=200)
-def get_todo_handler(id: int):
-    todo = todo_data.get(id)
-    if todo:
-        return todo
-    # HTTP STATUS CODE 반환 값 지정(import HTTPException)
-    raise HTTPException(status_code=404, detail="Not Found")
+@app.get("/todos/{todo_id}", status_code=200)
+def get_todo_handler(
+        todo_id: int,
+        session: Session = Depends(get_db),
+):
+    todo: ToDo = get_todo_by_todo_id(session=session, todo_id=todo_id)
 
-# RequestBody 대체(import BaseModel)
-class CreateToDoRequest(BaseModel):
-    id:int
-    contents: str
-    is_done: bool
+    if todo:
+        return ToDoSchema.from_orm(todo)
+    # HTTP STATUS CODE 반환 값 지정(import HTTPException)
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 # POST API
 @app.post("/todos", status_code=201)
-def create_todo_handler(request: CreateToDoRequest):
-    todo_data[request.id] = request.dict()
-    return todo_data[request.id]
+def create_todo_handler(
+        request: CreateToDoRequest,
+        session: Session = Depends(get_db),
+    ) -> ToDoSchema:
+    todo: ToDo = ToDo.create(request=request)
+    todo: ToDo = create_todo(session=session, todo=todo)
+    return ToDoSchema.from_orm(todo)
 
 # PATCH API
 @app.patch("/todos/{todo_id}", status_code=200)
 def update_todo_handler(
-        id: int,
+        todo_id: int,
         is_done: bool = Body(..., embed=True),
-):
-    todo = todo_data.get(id)
+        session: Session = Depends(get_db)
+    ):
+    todo: ToDo = get_todo_by_todo_id(session=session, todo_id=todo_id)
+    # todo check 후 ToDo가 존재한다면 Update
     if todo:
-        todo["is_done"] = is_done
-        return todo
-    raise HTTPException(status_code=404, detail="Not Found")
+        # update
+        todo.done() if is_done else todo.undone()
+        todo: ToDo = update_todo(session=session, todo=todo)
+        return ToDoSchema.from_orm(todo)
+    # HTTP STATUS CODE 반환 값 지정(import HTTPException)
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 # DELETE API
 @app.delete("/todos/{todo_id}", status_code=204)
-def delete_todo_handler(id: int):
-    # pop = id 값을 제거하고 반환
-    todo = todo_data.pop(id, None)
-    if todo:
-        return
-    raise HTTPException(status_code=404, detail="Not Found")
+def delete_todo_handler(
+        todo_id: int,
+        session: Session = Depends(get_db)
+):
+    todo: ToDo = get_todo_by_todo_id(session=session, todo_id=todo_id)
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="ToDo Not Found")
+
+    # 삭제 함수 호출
+    delete_todo(session=session, todo_id=todo_id)
